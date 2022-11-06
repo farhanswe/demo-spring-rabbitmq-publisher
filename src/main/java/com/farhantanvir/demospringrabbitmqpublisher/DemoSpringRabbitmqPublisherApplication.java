823baafaaeb658b882cb35ec10022e4e67e27952
Farhan Tanvir <farhanswe35@gmail.com>
2022-11-06 17:39:32 +0600

    upload project

---
+++ src/main/java/com/farhantanvir/demospringrabbitmqpublisher/DemoSpringRabbitmqPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.farhantanvir.demospringrabbitmqpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringRabbitmqPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringRabbitmqPublisherApplication.class, args);
+	}
+
+}
